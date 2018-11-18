a565072b809fb20e0f8beca381487180a8e46326
geliya <690674850@qq.com>
2018-11-18 17:00:01 +0800

    测试java

---
+++ maven_day02_1/src/main/java/com/itheima/controller/ItemsController.java
@@ -0,0 +1,28 @@
+package com.itheima.controller;
+
+import com.itheima.domain.Items;
+import com.itheima.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/items")
+public class ItemsController {
+
+    @Autowired
+    private ItemsService itemsService;
+
+    @RequestMapping("/findDetail")
+    public String findDetail(Model model){
+        Items items = itemsService.findById(1);
+        model.addAttribute("item",items);
+        return "itemDetail";
+    }
+    public void SSS(){
+        System.out.println("dsad");
+    }
+
+}
